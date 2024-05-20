0877d4af6237297f8fd4e7a6f2674d3f9ff7e6d7
tberkovac <tberkovac1@etf.unsa.ba>
2024-05-20 19:46:55 +0200

    Modified access token in sql connection

--- MediumLoadTesting/Data/AppDbContext.cs
+++ MediumLoadTesting/Data/AppDbContext.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 
 namespace MediumLoadTesting;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor accessor) : base(options)
+    {
+        var conn = Database.GetDbConnection() as SqlConnection;
+        conn.AccessToken = accessor.HttpContext.Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"];
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -15,7 +21,8 @@ public class AppDbContext : DbContext
 
     private Book[] GetBooks()
     {
-        return Enumerable.Range(1, 10).Select(index => new Book {
+        return Enumerable.Range(1, 10).Select(index => new Book
+        {
             BookId = index,
             Name = $"Name {index}",
             AuthorName = $"AuthorName {index}",
